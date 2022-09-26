fa2ccb2067697bda8684ae9a363c91432bb42f40
Thiago Bruno <tbgt35@hotmail.com>
2022-09-26 19:46:06 -0300

    colocando validações no códgo

--- eventos.js
+++ eventos.js
@@ -1,33 +1,37 @@
 
-const atualizarQuantidade = () => ( document.getElementById('numeros').innerHTML = `(${lista_tarefas.length})`);
+const atualizarQuantidade = () => ( document.getElementById('numeros').innerHTML = `(${buscar().length})`);
 
 const listaTarefas = () => ( document.getElementById('tarefas').innerHTML = 
-buscar().map( tarefa => `<div><input type="checkbox"> ${tarefa.titulo} 
+buscar().map( tarefa => `<div>
+<input type="checkbox"> ${tarefa.titulo}  
 <span class="badge 
-${tarefa.prioridade === 'Baixa' && 'bg-primary'} 
-${tarefa.prioridade === 'Média' && 'bg-warning'} 
-${tarefa.prioridade === 'Alta' && 'bg-danger'}"
+                    ${tarefa.prioridade === 'Baixa' && 'bg-primary'} 
+                    ${tarefa.prioridade === 'Média' && 'bg-warning'} 
+                    ${tarefa.prioridade === 'Alta' && 'bg-danger'}"
 >
  ${tarefa.prioridade}
- </span> </div>`).join(''));
+ </span></div>`).sort().join(''));
    
 const addTarefa = () => {
     event.preventDefault();
     
     let titulo = document.getElementById('input_novaTarefa').value;
-    let prioridade = document.getElementById('input-prioridade').value
+    let prioridade = document.getElementById('input-prioridade').value;
 
     if(titulo.trim() === ''){
         alert('Tarefa invalida')
         return;
     }
-    
-    if(lista_tarefas.includes(titulo)){
-        alert('Tarefa inválida')
+
+    if(listaTarefas().includes(titulo)){
+        alert('Tarefa repetida')
         return;
     }
 
+
     salvar(titulo, prioridade);
+
+    
 
     document.getElementById('input_novaTarefa').value='';
  
@@ -37,3 +41,8 @@ const addTarefa = () => {
 }
 
 listaTarefas();
+
+const apagar =() => {
+    document.getElementById('tarefas').innerHTML = '';
+    localStorage.removeItem('tarefas')
+}
